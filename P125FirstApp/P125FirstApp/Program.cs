1b8035289777532acd6eba8d9e0903118a3c2720
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-09-30 13:11:57 +0400

    Add files via upload

---
+++ P125FirstApp/P125FirstApp/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace P125FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //int num1 = 0;
+            //int num2 = 17000000;
+
+            //num1 = num1 + num2;  //num1=4+7=11
+            //num2 = num1 - num2; //num2=11-7=4
+            //num1 = num1 - num2;  //num1=11-4=7
+
+            //Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+
+            //int age = 1;
+
+            //switch (age)
+            //{
+            //    case 16:
+            //        Console.WriteLine("Yash 16");
+            //        break;
+            //    case 15:
+            //        Console.WriteLine("Yash 15");
+            //        break;
+            //    case 14:
+            //    case 13:
+            //    case 12:
+            //        Console.WriteLine("Pis deyil");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Bashqadir");
+            //        break;
+            //}
+
+            //if (age > 16)
+            //{
+            //    Console.WriteLine("Xosh gelmisiniz");
+            //}
+            //else if (age==16)
+            //{
+            //    Console.WriteLine("Az qalib");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Get boyuyun gelsin");
+            //}
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int[] arr = { 2, 15, 0, 32, 40 };
+            //Console.WriteLine(arr[3]);
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //int i = 1;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //} while (i<0);
+            //while (i<=10)
+            //{
+            //    //Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Console.WriteLine(i);
+
+            //for (;;)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+        }
+    }
+}
